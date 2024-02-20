a4effd2a9d713f6d99c98658843492505810ba0e
Linkie Link <linkielink.dev@gmail.com>
2024-02-20 16:11:07 +0100

    update the navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,13 +2,27 @@ import { NavLink } from 'react-router-dom'
 
 export default function Navigation() {
   return (
-    <nav className='fixed top-0 left-0 flex items-center justify-center w-full text-xl text-white bg-black h-25'>
-      <ul className='flex gap-10 list-none'>
-        <li>
-          <NavLink to='/'>Collection</NavLink>
+    <nav className='fixed top-0 left-0 z-10 flex items-center justify-center w-full px-4 text-xl text-white bg-black h-25'>
+      <ul className='flex flex-wrap list-none md:gap-10'>
+        <li className='w-full md:w-auto'>
+          <NavLink to='/' className='hover:underline'>
+            Collection
+          </NavLink>
         </li>
-        <li>
-          <NavLink to='/manifesto'>Manifesto</NavLink>
+        <li className='w-full md:w-auto'>
+          <NavLink to='/manifesto' className='hover:underline'>
+            Manifesto
+          </NavLink>
+        </li>
+        <li className='w-full md:w-auto'>
+          <a
+            href='https://twitter.com/IBC_Gangsters'
+            target='_blank'
+            rel='noreferrer'
+            className='hover:underline'
+          >
+            IBC Gangsters on X
+          </a>
         </li>
       </ul>
     </nav>
