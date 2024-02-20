bce437c9ba8f31355bfd8bbe5d2b366a3d36fbe8
Linkie Link <linkielink.dev@gmail.com>
2024-02-20 13:40:54 +0100

    update metadata

--- src/constants/pageMetadata.ts
+++ src/constants/pageMetadata.ts
@@ -8,11 +8,11 @@ const PAGE_METADATA = {
   manifesto: {
     title: 'IBC Gangsters - Manifesto',
     description:
-      "Yo, Space Fam! We're the first inscribed NFT collection on the Cosmos Hub. Embark on our cosmic journey and join the meme dream team! Gangster 4 Life!",
+      'Born in the gritty streets of Earth back in 2021, the IBC Gangsters were just faces in the crowd, grinding the 9-5, punching the clock, chasing that green while dreaming of a better life.',
     keywords: 'ibc, ibc gangsters, inscription, cosmos hub, cosmos, nft, nft collection, asteroids',
   },
   collection: {
-    title: 'IBC Gangsters - Manifesto',
+    title: 'IBC Gangsters - Collection',
     description:
       "Yo, Space Fam! We're the first inscribed NFT collection on the Cosmos Hub. Embark on our cosmic journey and join the meme dream team! Gangster 4 Life!",
     keywords: 'ibc, ibc gangsters, inscription, cosmos hub, cosmos, nft, nft collection, asteroids',
