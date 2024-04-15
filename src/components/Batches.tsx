190f974b2a8a04cc82a5dc08d4bac93bcda7deab
Linkie Link <linkielink.dev@gmail.com>
2024-04-15 21:35:06 +0200

    remove intro of batch overview

--- src/components/Batches.tsx
+++ src/components/Batches.tsx
@@ -3,7 +3,6 @@ import Batch from 'components/Batch'
 export default function Batches() {
   return (
     <div className='w-full max-w-[1024px] mx-auto py-20 '>
-      <h1 className='w-full pb-8 text-4xl text-center'>All IBC Gangsters Release Batches</h1>
       <Batch batchId='1' />
     </div>
   )
