d0ccfe58b1c5454467f22090b94f9d698d0ebfc2
Linkie Link <linkielink.dev@gmail.com>
2024-04-28 20:13:19 +0200

    fix cache for wallet balances

--- src/hooks/useBalance.ts
+++ src/hooks/useBalance.ts
@@ -1,11 +1,10 @@
-import { addresses } from 'constants/contants'
 import getBalance from 'hooks/fetch/getBalance'
 import useSWR from 'swr'
 
 export default function useBalance(denom: string, address: string) {
   const url = `https://cosmos-rest.publicnode.com/cosmos/bank/v1beta1/balances/${address}`
 
-  return useSWR(url && denom && `getBalance/${denom}`, () => getBalance(url, denom), {
+  return useSWR(url && denom && `getBalance/${address}/${denom}`, () => getBalance(url, denom), {
     revalidateOnFocus: false,
     refreshInterval: 10000,
   })
